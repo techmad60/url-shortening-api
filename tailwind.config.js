de26e38152e741afc15520ffd9c5100e1d16401d
Success Obasi <obasisuccess192@gmail.com>
2024-01-01 13:40:59 +0100

    Having problems with the illustration svg, I need help

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ export default {
         
         'desktop': "url('/images/pattern-bg-desktop.png')",
       },
+      colors: {
+        'cyan': "hsl(180, 66%, 49%)",
+
+        'dark-violet': "hsl(257, 27%, 26%)",
+
+        'red': "hsl(0, 87%, 67%)",
+
+        'gray': " hsl(0, 0%, 75%)",
+
+        'violet': " hsl(257, 7%, 63%)",
+
+        'very-dark-blue': "hsl(255, 11%, 22%)",
+        
+        'very-dark-violet': "hsl(260, 8%, 14%)"
+      },
     },
   },
   plugins: [],
